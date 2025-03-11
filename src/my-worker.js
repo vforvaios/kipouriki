fb5671a08bb60d8d58e423fd9faded93f8f66ed1
Vaios Anastasiadis <vaios_a@outlook.com>
2025-03-11 14:43:44 +0200

    Remove all caching

--- src/my-worker.js
+++ src/my-worker.js
@@ -8,13 +8,31 @@ import { NavigationRoute, registerRoute } from "workbox-routing";
 self.addEventListener("activate", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") self.skipWaiting();
   console.log("Hi Vaios service worker");
+
+  const expectedCacheNamesSet = new Set(Object.values(CURRENT_CACHES));
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!expectedCacheNamesSet.has(cacheName)) {
+            console.log("Hiiiiiiiiiiiiiiiiiii");
+            // If this cache name isn't present in the set of
+            // "expected" cache names, then delete it.
+            console.log("Deleting out of date cache:", cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
 });
 
 // self.__WB_MANIFEST is default injection point
-precacheAndRoute(self.__WB_MANIFEST);
+// precacheAndRoute(self.__WB_MANIFEST);
+precacheAndRoute(self.__WB_MANIFEST.filter((_) => false));
 
 // clean old assets
 cleanupOutdatedCaches();
 
 // to allow work offline
-registerRoute(new NavigationRoute(createHandlerBoundToURL("index.html")));
+// registerRoute(new NavigationRoute(createHandlerBoundToURL("index.html")));
